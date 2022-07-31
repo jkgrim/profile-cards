e5818307726d53bc61694afe093f5659b5c93898
jkgrim <j.grimshaw37@gmail.com>
2022-07-31 09:33:03 -0600

    added navbar, basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="navbar-container">
+        <div className="logo">Profile Cards</div>
+
+        <div className="nav-links-wrapper">
+          <a className="nav-button" href="#">
+            Nav button
+          </a>
+          <a className="nav-button" href="#">
+            Nav button
+          </a>
+          <a className="nav-button" href="#">
+            Nav button
+          </a>
+          <a className="nav-button" href="#">
+            Nav button
+          </a>
+        </div>
+
+        <div className="username">User Name</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
